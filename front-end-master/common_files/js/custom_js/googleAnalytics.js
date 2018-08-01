9f78caa0cbbaf58548a0fc2388d29dd3c425de83
Shanzid01 <shanzid.shaiham@gmail.com>
2018-08-02 01:15:47 +0600

    Design updates

--- front-end-master/common_files/js/custom_js/googleAnalytics.js
+++ front-end-master/common_files/js/custom_js/googleAnalytics.js
@@ -1,8 +1,10 @@
 //-----Google Analytics-------
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'UA-122703977-1');
+$(document).ready(()=>{
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-122703977-1');
+});
 
 
 
