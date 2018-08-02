686cb2629bbc4c44c55cfb9086e2c73629b7a481
Shanzid01 <shanzid.shaiham@gmail.com>
2018-08-03 03:17:09 +0600

    It's on the internet
    -hosted on firebase
    -uses cloudflare cdn for optimization
    -optimized the svgs and images and cut down content size by 73%; load times decreased from ~1min to roughly 5seconds

--- front-end-master/about_us/about.js
+++ front-end-master/about_us/about.js
@@ -62,17 +62,20 @@ function outClickListener(){
 }
 
 
+//this function is FILLED with bugs. have to change the whole thing later
 function getChild(elem, className){
 	var childs = [];
 	for (var i = 0; i < elem.childNodes.length; i++) {
-		if (elem.childNodes[i].className == className) {
-		  childs.push(elem.childNodes[i]);
+		if(elem.childNodes[i].className){
+			if (elem.childNodes[i].className.includes(className)) {
+			  childs.push(elem.childNodes[i]);
+			}
 		}
 	}
 	return childs;
 }
 
-function populatePop_Up(popUp, person){
+function populatePop_Up(popUp, person, populate){
 	let profile_data = getChild(popUp, 'profile-data')[0];
 		let img = profile_data.children[0];
 		let profile_info = getChild(profile_data, 'info')[0];
@@ -95,24 +98,72 @@ function populatePop_Up(popUp, person){
 				})
 		let main_bio = getChild(bio, 'main-bio')[0];
 
-	let getPersonChildren = person.getElementsByClassName('details')[0].getElementsByClassName.bind(person);
-	let img_link = person.getElementsByClassName('display-picture')[0].children[0].getAttribute('src');
-	img.setAttribute('src', img_link);
-	name.textContent = getPersonChildren('name')[0].children[0].textContent;
-	designation.textContent = getPersonChildren('designation')[0].textContent;
-	project_name.textContent = getPersonChildren('project-name')[0].textContent;
-	email.setAttribute('href', getPersonChildren('email')[0].textContent);
-	facebook.setAttribute('href', getPersonChildren('facebook')[0].textContent);
-	instagram.setAttribute('href', getPersonChildren('instagram')[0].textContent);
-	twitter.setAttribute('href', getPersonChildren('twitter')[0].textContent);
-	line_title.forEach(function(elem, i){
-		elem.textContent = getPersonChildren('line')[i].getElementsByClassName('title')[0].textContent;
-	})
-	line_p.forEach(function(elem, i){
-		elem.textContent = getPersonChildren('line')[i].getElementsByClassName('data')[0].textContent;
-	})
-	main_bio.textContent = getPersonChildren('main-bio')[0].textContent;
+	if(populate){
+		let getPersonChildren = person.getElementsByClassName('details')[0].getElementsByClassName.bind(person);
+		let img_link = person.getElementsByClassName('display-picture')[0].children[0].getAttribute('src');
+		img.setAttribute('src', img_link);
+		name.textContent = getPersonChildren('name')[0].children[0].textContent;
+		designation.textContent = getPersonChildren('designation')[0].textContent;
+		project_name.textContent = getPersonChildren('project-name')[0].textContent;
+
+		if(getPersonChildren('twitter')[0].textContent == ""){
+			twitter.classList.add('hide');
+		}else{
+			twitter.classList.remove('hide');
+			twitter.setAttribute('href', getPersonChildren('twitter')[0].textContent);
+		}
+
+		if(getPersonChildren('facebook')[0].textContent == ""){
+			facebook.classList.add('hide');
+		}else{
+			console.log("ne");
+			facebook.classList.remove('hide');
+			console.log(facebook.classList);
+			facebook.setAttribute('href', getPersonChildren('facebook')[0].textContent);
+		}
+
+		if(getPersonChildren('email')[0].textContent == ""){
+			email.classList.add('hide');
+		}else{
+			email.classList.remove('hide');
+			email.setAttribute('href', 'mailto:'+getPersonChildren('email')[0].textContent);
+		}
+
+		if(getPersonChildren('instagram')[0].textContent == ""){
+			instagram.classList.add('hide');
+		}else{
+			instagram.classList.remove('hide');
+			instagram.setAttribute('href', getPersonChildren('instagram')[0].textContent);
+		}
+
+		line_title.forEach(function(elem, i){
+			elem.innerHTML = getPersonChildren('line')[i].getElementsByClassName('title')[0].innerHTML;
+		})
+		line_p.forEach(function(elem, i){
+			elem.innerHTML = getPersonChildren('line')[i].getElementsByClassName('data')[0].innerHTML;
+		})
+		main_bio.innerHTML = getPersonChildren('main-bio')[0].innerHTML;
+	}else{
+		let getPersonChildren = person.getElementsByClassName('details')[0].getElementsByClassName.bind(person);
+		let img_link = person.getElementsByClassName('display-picture')[0].children[0].getAttribute('src');
+		img.setAttribute('src', "");
+		name.textContent = "";
+		designation.textContent = "";
+		project_name.textContent = "";
+		email.setAttribute('href', "#");
+		facebook.setAttribute('href', "#");
+		instagram.setAttribute('href', "#");
+		twitter.setAttribute('href', "#");
+		line_title.forEach(function(elem, i){
+			elem.textContent = "";
+		})
+		line_p.forEach(function(elem, i){
+			elem.textContent = "";
+		})
+		main_bio.innerHTML = "";
+	}
 }
+
 
 $(document).ready(function(){
 	$('.div-name').hover(function(){
@@ -121,16 +172,18 @@ $(document).ready(function(){
 		$('.div-selector-container').addClass('div'+(divIndex+1));
 		$('.division-container:first-child').addClass('divC'+(divIndex+1))
 	})
-
+	var currentPerson = "";
 	$(".person").click(function(){
 		$('.pop-up-container')[0].classList.add("pop-up-shown");
 		$('.pop-up-container')[0].classList.remove("pop-up-hidden");
-		populatePop_Up(document.getElementsByClassName('pop-up')[0], this);
+		populatePop_Up(document.getElementsByClassName('pop-up')[0], this, true);
+		currentPerson = this;
 	})
 
 	$(".cross").click(function(){
 		$('.pop-up-container')[0].classList.remove("pop-up-shown",
 			$('.pop-up-container')[0].classList.add("pop-up-hidden"));
+			populatePop_Up(document.getElementsByClassName('pop-up')[0], currentPerson, false);
 	})
 
 })
