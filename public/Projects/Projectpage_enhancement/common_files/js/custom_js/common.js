1069acf118931c901bce9acce8f8daf9d5999a35
DESKTOP-72NQ42C\mahbuba <tasmin153@gmail.com>
2018-11-04 21:50:03 +0600

    Updated project page, adjusted to the existing website.

--- public/Projects/Projectpage_enhancement/common_files/js/custom_js/common.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(()=>{
-    $('img').each(function () {
-        var imgurl = $(this).attr('data-src');
-        if (imgurl && $(this).css('display')!='none') {
-            $(this).attr('src', imgurl);
-        }
-    });
-    $('.secure-img').bind('contextmenu', function(e) {return false;});
-});
